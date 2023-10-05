e4091318c285d4a7c7149da951f3a193526bb04b
Nick Hobart <nick@hobart.io>
2023-10-05 09:10:40 -0700

    chore: updating main.py to pass psf black

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ from jira import JIRA
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-def get_branch_name_based_on_jira_ticket(jira_server, jira_email, jira_api_token, ticket_id):
+
+def get_branch_name_based_on_jira_ticket(
+    jira_server, jira_email, jira_api_token, ticket_id
+):
     # Ensure the ticket ID has a prefix before proceeding
-    if '-' not in ticket_id:
+    if "-" not in ticket_id:
         logging.error("Ticket ID must include a prefix followed by a hyphen.")
         exit(1)
 
@@ -20,7 +23,13 @@ def get_branch_name_based_on_jira_ticket(jira_server, jira_email, jira_api_token
     ticket = jira.issue(ticket_id)
 
     issue_type = ticket.fields.issuetype.name.lower()
-    branch_prefix = "feature" if issue_type == "story" else "fix" if issue_type == "bug" else "issue"
+    branch_prefix = (
+        "feature"
+        if issue_type == "story"
+        else "fix"
+        if issue_type == "bug"
+        else "issue"
+    )
 
     branch_name = f"{branch_prefix}/{ticket_id}-{ticket.fields.summary.replace(' ', '_').replace('/', '').replace(':', '').replace('?', '').lower()}"
 
@@ -29,6 +38,7 @@ def get_branch_name_based_on_jira_ticket(jira_server, jira_email, jira_api_token
         branch_name = branch_name[:100]
 
     return branch_name
+
 
 def create_git_branch_and_set_upstream(branch_name):
     repo = Repo()
@@ -52,26 +62,35 @@ def create_git_branch_and_set_upstream(branch_name):
 
     logging.info(f"Pushing the new branch '{branch_name}' and setting the upstream...")
     origin.push(branch_name, set_upstream=True)
-    logging.info(f"Successfully pushed the new branch '{branch_name}' and set the upstream.")
+    logging.info(
+        f"Successfully pushed the new branch '{branch_name}' and set the upstream."
+    )
+
 
 if __name__ == "__main__":
-    jira_server = os.environ.get('JIRA_SERVER', 'https://clearcover.atlassian.net/')
-    jira_email = os.environ.get('JIRA_EMAIL')
-    jira_api_token = os.environ.get('JIRA_API_TOKEN')
+    jira_server = os.environ.get("JIRA_SERVER", "https://clearcover.atlassian.net/")
+    jira_email = os.environ.get("JIRA_EMAIL")
+    jira_api_token = os.environ.get("JIRA_API_TOKEN")
 
     if not jira_email or not jira_api_token:
-        logging.error("JIRA_EMAIL and JIRA_API_TOKEN environment variables must be set.")
+        logging.error(
+            "JIRA_EMAIL and JIRA_API_TOKEN environment variables must be set."
+        )
         exit(1)
 
     # Check for a command-line argument (JIRA ticket ID)
     if len(sys.argv) == 2 and re.match(r"([A-Z]+-)?\d+", sys.argv[1]):
         ticket_id = sys.argv[1]
     else:
-        ticket_id = input("Enter the JIRA ticket ID (with the prefix, e.g., 'SRE-1234'): ")
+        ticket_id = input(
+            "Enter the JIRA ticket ID (with the prefix, e.g., 'SRE-1234'): "
+        )
 
     if not re.match(r"([A-Z]+-)?\d+", ticket_id):
         logging.error("Invalid ticket ID format.")
         exit(1)
 
-    branch_name = get_branch_name_based_on_jira_ticket(jira_server, jira_email, jira_api_token, ticket_id)
+    branch_name = get_branch_name_based_on_jira_ticket(
+        jira_server, jira_email, jira_api_token, ticket_id
+    )
     create_git_branch_and_set_upstream(branch_name)
